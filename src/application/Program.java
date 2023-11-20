a497315bda6cff84f6813d6ca3c3ef86f23e499f
victorferraz364 <victorferraz364@gmail.com>
2023-11-20 12:24:51 -0300

    UI board game

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 	public static void main (String[] args) {
 
-		Board board = new Board(2,4);
+		ChessMatch chessMacth = new ChessMatch();
+		UI.printBoard(chessMacth.getPieces());
 		
 		
 	}
+	
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -5,7 +5,7 @@ public class Piece {
 	protected Position position;
 	private Board board;
 	
-	public Piece(Position position, Board board) {
+	public Piece(Board board) {
 		this.board = board;
 	}
 
